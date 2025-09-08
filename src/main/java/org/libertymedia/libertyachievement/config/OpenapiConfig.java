55e24f82d5d4560048e7ec852b88b30ccf1ba40a
Xen-alpha <hky7548@naver.com>
2025-09-08 22:13:49 +0900

    [Fix] fix uri security config error

--- src/main/java/org/libertymedia/libertyachievement/config/OpenapiConfig.java
+++
@@ -1,7 +0,0 @@
-package org.libertymedia.libertyachievement.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenapiConfig {
-}
--- src/main/java/org/libertymedia/libertyachievement/config/SecurityConfig.java
+++ src/main/java/org/libertymedia/libertyachievement/config/SecurityConfig.java
@@ -21,9 +21,9 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(
                 (auth) -> auth
-                        .requestMatchers("/achievement/v1/list/**", "/login", "/login/**", "/logout", "/", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/achievement/v1/list/**", "/login", "/login/**", "/logout", "/", "/swagger-ui/index.html", "/v3/**").permitAll()
                         .requestMatchers("/achievement/v1/achieve", "/achievement/v1/edit", "/achievement/v1/rate","/achievement/v1/talk", "/achievement/v1/file", "/achievement/v1/game/**", "/user/**").hasRole("BASIC")
-                        .requestMatchers("/achievement/v1/achieve","/achievement/v1/addtion","/achievement/v1/deletion", "/achievement/v1/edit", "/achievement/v1/rate","/achievement/v1/talk", "/achievement/v1/file", "/achievement/v1/game/**", "/user/**").hasRole("ADVANCED")
+                        .requestMatchers("/achievement/v1/achieve","/achievement/v1/addition","/achievement/v1/deletion", "/achievement/v1/edit", "/achievement/v1/rate","/achievement/v1/talk", "/achievement/v1/file", "/achievement/v1/game/**", "/user/**").hasRole("ADVANCED")
                         .anyRequest().authenticated()
         ).oauth2Login(oauth2 -> oauth2
                 .userInfoEndpoint(userInfo -> userInfo.userService(userService)
