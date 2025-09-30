da5cad44325b2d0f312df66d9866e4881ce77619
Xen-alpha <hky7548@naver.com>
2025-09-30 15:48:23 +0900

    [Fix] restore custom login filter

--- src/main/java/org/libertymedia/libertyachievement/config/filter/JWTFilter.java
+++ src/main/java/org/libertymedia/libertyachievement/config/filter/JWTFilter.java
@@ -15,13 +15,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.libertymedia.libertyachievement.util.JwtUtil;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
