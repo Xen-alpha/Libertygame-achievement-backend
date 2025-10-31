a7f5b6ebe77261618946980e9e42ffe02463d3b9
Xen-alpha <hky7548@naver.com>
2025-10-31 17:03:02 +0900

    [Fix] fix jwt filter

--- src/main/java/org/libertymedia/libertyachievement/config/SecurityConfig.java
+++ src/main/java/org/libertymedia/libertyachievement/config/SecurityConfig.java
@@ -50,8 +50,7 @@ public class SecurityConfig {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception { // 세션 방식 로그인
         http.oauth2Login(oauth2 -> oauth2
                 .permitAll().userInfoEndpoint(userInfoEP -> userInfoEP.userService(userService)
-                ).successHandler(authSuccessHandler
-                )
+                ).successHandler(authSuccessHandler)
         ).csrf(AbstractHttpConfigurer::disable
         ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // v0.5.3에서 난 결론: 세션 아닌 JWT 인증이어야 도전과제 서버가 본 서버와 양립 가능한 것으로 결론을 내림
         ).logout(logout -> logout.permitAll().deleteCookies("AccessTOKEN").logoutSuccessHandler(
